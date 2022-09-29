a0e32fd4206b4f78fb340b30787bc4617fbfe4fa
Dannielle Jones <dannielle.jones1@my.jcu.edu.au>
2022-09-29 17:17:45 +1000

    Add commit_messages text file; update password_stars simple to use constants

--- prac_02/password_stars_simple.py
+++ prac_02/password_stars_simple.py
@@ -2,10 +2,10 @@
 CP1404 | Prac_02 | Password Stars
 Get user password and check length is longer than minimum length.
 """
+MINIMUM_LENGTH = 8
 
 password = input("Password: ")
-minimum_length = 8
-while len(password) < minimum_length:
+while len(password) < MINIMUM_LENGTH:
     print("Error")
     password = input("Password: ")
 print("*" * len(password))
